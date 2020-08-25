8c261d6c3491a820a4b72fa98bfb682feebb9242
ELICHAYBARAK <elichayb@il.ibm.com>
2020-08-25 22:33:08 +0300

    add copy rights... Fix some small bugs

--- babyd/src/main/java/com/babyd/babyd/repositories/ParentRepository.java
+++ babyd/src/main/java/com/babyd/babyd/repositories/ParentRepository.java
@@ -1,3 +1,13 @@
+/*
+ * ----------------------------------------------------------------------------
+ *  (C) Copyright Elichy Barak 2020
+ *
+ *  The source code for this program is not published or other-
+ * wise divested of its trade secrets, irrespective of what has
+ * been deposited with the U.S. Copyright Office.
+ * ----------------------------------------------------------------------------
+ */
+
 package com.babyd.babyd.repositories;
 
 import com.babyd.babyd.exceptions.EtAuthException;
